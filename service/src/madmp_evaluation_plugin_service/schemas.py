941ac67cf8fefe716ddb685f4bb796c7ea17da2d
Marek Suchánek <marek.suchanek@gmail.com>
2026-02-13 13:23:20 +0100

    Use camelCase in DTOs

--- service/src/madmp_evaluation_plugin_service/schemas.py
+++ service/src/madmp_evaluation_plugin_service/schemas.py
@@ -1,7 +1,20 @@
 import pydantic
 
 
-class EvaluationRequest(pydantic.BaseModel):
+def to_camel(s: str) -> str:
+    parts = s.split('_')
+    return parts[0] + ''.join(word.capitalize() for word in parts[1:])
+
+
+class BaseModel(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        extra='forbid',
+    )
+
+
+class EvaluationRequest(BaseModel):
     api_url: str
     project_uuid: str
     user_token: str
@@ -9,14 +22,14 @@ class EvaluationRequest(pydantic.BaseModel):
     test: str | None = None
 
 
-class EvaluationResponse(pydantic.BaseModel):
+class EvaluationResponse(BaseModel):
     ok: bool
     evaluations: list[dict] = pydantic.Field(default_factory=list)
     madmp: dict | None
     message: str | None = pydantic.Field(default=None)
 
 
-class FormDataResponse(pydantic.BaseModel):
+class FormDataResponse(BaseModel):
     ok: bool
     benchmarks: list[dict] = pydantic.Field(default_factory=list)
     tests: list[dict] = pydantic.Field(default_factory=list)
