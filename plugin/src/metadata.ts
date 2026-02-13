adbcaf09313dddd34cbf0ce86999d842766a3e38
Jan Slifka <jan.slifka@gmail.com>
2026-02-13 09:50:58 +0100

    Set version and package name

--- plugin/src/metadata.ts
+++ plugin/src/metadata.ts
@@ -3,6 +3,6 @@ import { PluginMetadata } from '@ds-wizard/plugin-sdk/types'
 export const pluginMetadata: PluginMetadata = {
     uuid: '8d372e4a-6145-4863-9831-82eb0ead1195',
     name: 'maDMP Evaluation',
-    version: '1.0.0',
+    version: '0.1.0',
     description: 'This is a DSW Plugin created from the template.',
 }
