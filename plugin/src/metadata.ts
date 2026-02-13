4855b60640d28a2c0caebd5bf2f29ff4e5b925e0
Jan Slifka <jan.slifka@gmail.com>
2026-02-13 18:29:16 +0100

    Add plugin description

--- plugin/src/metadata.ts
+++ plugin/src/metadata.ts
@@ -4,5 +4,5 @@ export const pluginMetadata: PluginMetadata = {
     uuid: '8d372e4a-6145-4863-9831-82eb0ead1195',
     name: 'maDMP Evaluation',
     version: '0.1.0',
-    description: 'This is a DSW Plugin created from the template.',
+    description: 'Runs maDMP evaluations for a project and displays validation results.',
 }
